ef427321803f70e7f4e403e3af6d3e63e9fe820d
MitjaK004 <klajnsek.mitja@gmail.com>
2026-03-10 15:53:35 +0100

    osnovna implementacija routerja

---
+++ code/backend/controllers/bugController.js
@@ -0,0 +1,39 @@
+//var BugModel = // TO DO
+
+module.exports = {
+
+    /**
+     * userController.list()
+     */
+    list: function (req, res) {
+        //TO DO
+    },
+
+    /**
+     * userController.show()
+     */
+    show: function (req, res) {
+        //TO DO
+    },
+
+    /**
+     * userController.create()
+     */
+    create: function (req, res) {
+        //TO DO
+    },
+
+    /**
+     * userController.update()
+     */
+    update: function (req, res) {
+        //TO DO
+    },
+
+    /**
+     * userController.remove()
+     */
+    remove: function (req, res) {
+        //TO DO
+    },
+};
--- code/backend/server.js
+++ code/backend/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+var router = require('./routes/routes');
+var userRouter = require('./routes/userRoutes');
+var promptRouter = require('./routes/promptRoutes');
+var bugRouter = require('./routes/bugRoutes');
 
 dotenv.config();
 const app = express();
@@ -15,8 +19,9 @@ mongoose.connect(mongoUri)
     .then(() => console.log('Povezan na MongoDB!'))
     .catch(err => console.error('Napaka pri povezavi na DB:', err));
 
-app.get('/', (req, res) => {
-    res.json({ message: "ProkrastinatorGPT API deluje!", status: "online" });
-});
+app.use('/', router);
+app.use('/users', userRouter);
+app.use('/prompt', promptRouter);
+app.use('/bug', bugRouter);
 
 const PORT = process.env.PORT || 5000;
