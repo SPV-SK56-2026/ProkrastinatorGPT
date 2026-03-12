41e2f98e132b65b35b61dfcdc3f718c07e168c85
TimAndrejc <91323249+TimAndrejc@users.noreply.github.com>
2026-03-12 19:47:39 +0100

    Fixed merge problems

--- code/frontend/src/login.tsx
+++ code/frontend/src/login.tsx
@@ -1,76 +1,56 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
-import Header from './header'
-
-const observer = new ResizeObserver(() => {
-  window.parent.postMessage({ 
-    type: "RESIZE_IFRAME", 
-    height: document.body.scrollHeight 
-  }, "*");
-});
-observer.observe(document.body);
-
+import LayoutWrapper from './components/LayoutWrapper'
+import PageHeader from './components/PageHeader'
+import FormInput from './components/FormInput'
+import PrimaryButton from './components/PrimaryButton'
 
 interface Props {
   setPage?: (page: string) => void;
 }
 
 function Login({ setPage }: Props) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const params = new URLSearchParams(window.location.search);
+  const noAssignment = params.get("noAssignment");
+
   return (
-    <>
-        <Header setPage={setPage} />
-        <div id="titleContainer">
-            <span id="title">Prijava</span>
-            <img src="icons/login.png" id="registerIcon" alt="register" />
-        </div>
-        <div className='formContainer'>
-            <form>
-                <div className="registerContainer">
-                    <label className="registerLabel">Email</label>
-                    <input
-                    className='registerInput'
-                    type='email'
-                    />
-                </div>
-
-                <div className="registerContainer">
-                    <label className="registerLabel">Geslo</label>
-                    <input
-                    className='registerInput'
-                    type='password'
-                    />
-                </div>
-                
-                <div className="registerContainer">
-                    <button className="btnSubmit">Prijava</button>
-                    <div className="linksContainer">
-                        <a
-                        href='#'
-                        className='linkToRegister'
-                          onClick={() => setPage?.("register")} >
-                          
-                        Še nimaš računa? Registriraj se
-                        </a>
-                        <a
-                        href='#'
-                        className='linkToPassChange'
-                         onClick={() => setPage?.("forgot")} >
-                        Pozabljeno geslo?
-                        </a>
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div className="btnWrapper">
-            <button
-                className="btnClose"
-                onClick={() => window.parent.postMessage({ type: "CLOSE_IFRAME" }, "*")}
-            >
-                Zapri
-            </button>
+    <LayoutWrapper isNoAssignment={!!noAssignment} setPage={setPage}>
+      <PageHeader 
+        title="Prijava" 
+        iconSrc="icons/login.png" 
+        iconId="registerIcon" 
+      />
+      <div className='formContainer'>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <FormInput 
+            label="Email" 
+            type="email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+          <FormInput 
+            label="Geslo" 
+            type="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+          <div className="registerContainer">
+            <PrimaryButton>Prijava</PrimaryButton>
+            <div className="linksContainer">
+              <a href='#' className='linkToRegister' onClick={() => setPage?.("register")}>
+                Še nimaš računa? Registriraj se
+              </a>
+              <a href='#' className='linkToPassChange' onClick={() => setPage?.("forgot")}>
+                Pozabljeno geslo?
+              </a>
             </div>
-
-    </>
+          </div>
+        </form>
+      </div>
+    </LayoutWrapper>
   )
 }
+
 export default Login
